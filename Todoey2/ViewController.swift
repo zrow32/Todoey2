1adad584d744f5a9ac931135b477cd5e70ef1094
zrow32 <z.rows32@gmail.com>
2019-07-01 14:17:40 +0300

    Initial Commit

---
+++ Todoey2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey2
+//
+//  Created by Vladimir Zhirov on 7/1/19.
+//  Copyright © 2019 Vladimir Zhirov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
